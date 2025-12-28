d7c7bb2a27bb660d4c417f865050fd169d7870fe
Override <122590935+TuringProblem@users.noreply.github.com>
2025-12-28 14:18:05 -0500

    feat: starting to work on the structure but wanted you (ben) to be able to see the change

--- open-search/App.tsx
+++ open-search/App.tsx
@@ -1,15 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, /*Text,*/ View } from 'react-native';
 import Button from './components/button/button';
 
 const buttons = [
   {
-    text: 'Button 1',
+    text: 'Computer Science',
     onPress: () => console.log('Button 1 pressed'),
   },
   {
-    text: 'Button 2',
+    text: 'Mathematics',
     onPress: () => console.log('Button 2 pressed'),
+  },
+  {
+    text: 'Science',
+    onPress: () => console.log('Button 3 pressed'),
   },
 ]
 
@@ -19,7 +23,7 @@ export default function App() {
       {buttons.map((button, index) => (
         <Button key={index} text={button.text} onPress={button.onPress} />
       ))}
-      <Text>Hello World</Text>
+      {/*<Text>Hello World</Text>*/}
       <StatusBar style="auto" />
     </View>
   );
--- open-search/components/button/button.tsx
+++ open-search/components/button/button.tsx
@@ -7,16 +7,7 @@ interface ButtonProps {
   onPress: () => void;
 }
 
-const buttons = [
-  {
-    text: 'Button 1',
-    onPress: () => console.log('Button 1 pressed'),
-  },
-  {
-    text: 'Button 2',
-    onPress: () => console.log('Button 2 pressed'),
-  },
-]
+
 
 const Button: React.FC<ButtonProps> = ({ text, onPress }) => {
 
@@ -33,16 +24,18 @@ const styles = StyleSheet.create({
   buttonContainer: {
     backgroundColor: '#1C1C1E',
     borderRadius: 12,
-    padding: 16,
-    marginBottom: 16,
+    padding: 10,
+    marginBottom: 2,
     borderWidth: 1,
     borderColor: '#333',
+    width: '100%',
   },
   buttonText: {
     fontSize: 18,
     fontWeight: '600',
     color: '#FFF',
     marginBottom: 16,
+    textAlign: 'center',
   },
 });
 
